ccbdb941db04deaf6763ba0d5b463169062eb22d
sravya <podilasravya7052@gmail.com>
2025-12-15 13:31:25 +0530

    added search

--- src/app/flight/search/search.ts
+++ src/app/flight/search/search.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { FlightService } from '../flight.service';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './search.html',
-  styleUrl: './search.css',
+  styleUrl: './search.css'
 })
 export class Search {
 
+  private fb = inject(FormBuilder);
+  private flightService = inject(FlightService);
+  private cdr = inject(ChangeDetectorRef); 
+
+  flights: any[] = [];
+  errorMsg = '';
+
+  searchForm = this.fb.group({
+    fromPlace: ['', Validators.required],
+    toPlace: ['', Validators.required],
+    journeyDate: ['', Validators.required],
+    tripType: ['ONE_WAY', Validators.required]
+  });
+
+  search() {
+    if (this.searchForm.invalid) {
+      this.searchForm.markAllAsTouched();
+      return;
+    }
+
+    this.flightService.searchFlights(this.searchForm.value)
+      .subscribe({
+        next: (data) => {
+          this.flights = data;
+          this.errorMsg = '';
+          this.cdr.detectChanges(); 
+        },
+        error: () => {
+          this.errorMsg = 'No flights found';
+          this.flights = [];
+          this.cdr.detectChanges();
+        }
+      });
+  }
+
+  newSearch() {
+    this.flights = [];
+    this.cdr.detectChanges();
+  }
 }
