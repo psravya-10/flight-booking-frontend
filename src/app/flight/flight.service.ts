ee1eb345ee5de78bba168718d51d24a7be409953
sravya <podilasravya7052@gmail.com>
2025-12-15 13:30:20 +0530

    added service.ts

---
+++ src/app/flight/flight.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightService {
+
+  private baseUrl = environment.API_BASE_URL;
+
+  constructor(private http: HttpClient) {}
+
+  searchFlights(payload: any) {
+    return this.http.post<any[]>(
+      `${this.baseUrl}/api/flight/search`,
+      payload
+    );
+  }
+}
