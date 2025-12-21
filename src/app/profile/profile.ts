17d472808d28eab36b5299b07df2d45315d462b2
sravya <podilasravya7052@gmail.com>
2025-12-21 17:27:35 +0530

    remove history from profile:

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -14,17 +14,12 @@ import { ChangeDetectorRef } from '@angular/core';
   styleUrls: ['./profile.css']
 })
 export class Profile {
-   constructor(
-    private cdr:ChangeDetectorRef
-  ){}
-
-  private bookingService = inject(BookingService);
-  private router = inject(Router);
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private router: Router
+  ) { }
 
   email = '';
-  bookings: any[] = [];
-  errorMsg = '';
-
   ngOnInit() {
     const email = localStorage.getItem('email');
 
@@ -32,61 +27,8 @@ export class Profile {
       this.router.navigate(['/login']);
       return;
     }
-
     this.email = email;
-    this.loadHistory();
   }
-
-  loadHistory() {
-    this.bookingService.getHistory(this.email).subscribe({
-      next: (data) => {
-        this.bookings = data;
-        this.cdr.detectChanges();
-      },
-      error: () => {
-        this.errorMsg = 'Failed to load booking history';
-      }
-    });
-  }
-  getStatus(b: any): string {
-  if (b.cancelled === true) return 'CANCELLED';
-
-  const today = new Date();
-  const journey = new Date(b.journeyDate);
-
-  return journey < today ? 'COMPLETED' : 'UPCOMING';
-}
-
-
-markAsCancelled(pnr: string) {
-  const booking = this.bookings.find(b => b.pnr === pnr);
-  if (booking) {
-    booking.cancelled = true;
-  }
-}
-
-cancelBooking(pnr: string) {
-  if (!confirm(`Cancel ticket ${pnr}?`)) return;
-
-  this.bookingService.cancelBooking(pnr).subscribe({
-    next: () => {
-      
-      const booking = this.bookings.find(b => b.pnr === pnr);
-      if (booking) {
-        booking.cancelled = true;
-      }
-    },
-    error: (err) => {
-      if (err?.error?.message?.toLowerCase().includes('already')) {
-        const booking = this.bookings.find(b => b.pnr === pnr);
-        if (booking) {
-          booking.cancelled = true;
-        }
-      }
-    }
-  });
-}
-
 
   logout() {
     localStorage.clear();
